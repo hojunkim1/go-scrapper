407cb61119d8c39bdd26ce72e7e648c79403fa69
hojunkim1 <76956067+hojunkim1@users.noreply.github.com>
2021-07-13 14:29:38 +0900

    made practice pkg: account

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -1,10 +1,17 @@
 package accounts
 
+import (
+	"errors"
+	"fmt"
+)
+
 // Account struct
 type Account struct {
 	owner string
 	balance int
 }
+
+var errNoMoney = errors.New("can't withdraw you are poor")
 
 // NewAccount creates Account
 func NewAccount(owner string) *Account {
@@ -13,6 +20,34 @@ func NewAccount(owner string) *Account {
 }
 
 // Deposit x amount on your account
-func (a Account) Deposit(amount int) {
+func (a *Account) Deposit(amount int) {
 	a.balance += amount
+}
+
+// Balance of your account
+func (a Account) Balance() int {
+	return a.balance
+}
+
+//Withdraw x amount from your account
+func (a *Account) Withdraw(amount int) error {
+	if a.balance < amount  {
+		return errNoMoney
+	}
+	a.balance -= amount
+	return nil
+}
+
+// ChangeOwner of the account
+func (a *Account) CahngeOwner(newOwner string) {
+	a.owner = newOwner
+}
+
+// Owner of the account
+func (a Account) Owner() string {
+	return a.owner
+}
+
+func (a Account) String() string {
+	return fmt.Sprint(a.Owner(), "'s account. \nHas: ", a.Balance())
 }
--- main.go
+++ main.go
@@ -1,11 +1,5 @@
 package main
 
-import (
-	"github.com/hojunkim1/learngo/accounts"
-)
-
 
 func main() {
-	account := accounts.NewAccount("hojun")
-	account.Deposit(10)
 }
