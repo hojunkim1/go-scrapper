9c0e9e9ec189a4253506f3ceb22581da58c9960e
Hojun Kim <76956067+hojunkim1@users.noreply.github.com>
2023-08-10 22:17:06 +0900

    restart

---
+++ account/account.go
@@ -0,0 +1,51 @@
+package account
+
+import (
+	"errors"
+	"fmt"
+)
+
+// Account struct
+type Account struct {
+	owner   string
+	balance int
+}
+
+// New creates Account
+func New(owner string) *Account {
+	account := Account{owner: owner, balance: 0}
+	return &account
+}
+
+// Owner of the account
+func (a Account) Owner() string {
+	return a.owner
+}
+
+// Balance of your account
+func (a Account) Balance() int {
+	return a.balance
+}
+
+// ChangeOwner of the account
+func (a *Account) ChangeOwner(newOwner string) {
+	a.owner = newOwner
+}
+
+// Deposit x amount on your account
+func (a *Account) Deposit(amount int) {
+	a.balance += amount
+}
+
+// Withdraw from your account
+func (a *Account) Withdraw(amount int) error {
+	if a.balance < amount {
+		return errors.New("Can't withdraw")
+	}
+	a.balance -= amount
+	return nil
+}
+
+func (a Account) String() string {
+	return fmt.Sprint(a.Owner(), "'s account\nhas : ", a.Balance())
+}
--- main.go
+++ main.go
@@ -1,29 +1,5 @@
 package main
 
-import (
-	"os"
-	"strings"
-
-	"github.com/hojunkim1/learngo/pkg/scrapper"
-	"github.com/labstack/echo"
-)
-
-const fileName string = "jobs.csv"
-
 func main() {
-	e := echo.New()
-	e.GET("/", handleHome)
-	e.POST("/scrape", handleScrape)
-	e.Logger.Fatal(e.Start(":1323"))
-}
 
-func handleHome(c echo.Context) error {
-	return c.File("home.html")
-}
-
-func handleScrape(c echo.Context) error {
-	defer os.Remove(fileName)
-	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
-	scrapper.Scrape(term)
-	return c.Attachment(fileName, fileName)
 }
