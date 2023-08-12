e59de972b795814dfa851fa4f2d1a246b90c6720
Hojun Kim <76956067+hojunkim1@users.noreply.github.com>
2023-08-12 21:31:32 +0900

    start scrapping

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "github.com/hojunkim1/go-scrapper/checkurl"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+var baseURL string = "https://kr.indeed.com/jobs?q=python"
 
 func main() {
-	checkurl.CheckURL()
+	pages := getPages()
+	fmt.Println(pages)
+}
+
+func getPages() int {
+	res, err := http.Get(baseURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	doc.Find(".ecydgvn0").Each(func(i int, s *goquery.Selection) {
+		fmt.Println(s.Html())
+	})
+
+	fmt.Println(doc)
+
+	return 0
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func checkCode(res *http.Response) {
+	if res.StatusCode != 200 {
+		log.Fatalln("Request failed with status:", res.StatusCode)
+	}
 }
